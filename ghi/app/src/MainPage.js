5851985d349c19a34cb01e36d5ef0f73bfaf6ed0
mariscript <marisonmunoz@gmail.com>
2023-02-19 17:47:51 -0500

    added images

--- ghi/app/src/MainPage.js
+++ ghi/app/src/MainPage.js
@@ -1,15 +1,16 @@
 function MainPage() {
   return (
     <div className="px-4 py-5 my-5 text-center">
-      <img className="sm-image"
+      <img
+        className="sm-image"
         src="https://ultranyc.com/wp-content/uploads/2020/03/giphy-56-1.gif"
-        alt="wardrobe gif" width="600" 
+        alt="wardrobe gif"
+        width="600"
       />
-      <h1 className="display-5 fw-bold">WARDROBIFY!</h1>
+      <h1 className="display-5 fw-bold">Fitted Sole</h1>
       <div className="col-lg-6 mx-auto">
         <p className="lead mb-4">
-          Need to keep track of your shoes and hats? We have
-          the solution for you!
+          The perfect fit for your hat and shoe collection.
         </p>
       </div>
     </div>
--- ghi/app/src/Nav.js
+++ ghi/app/src/Nav.js
@@ -1,41 +1,68 @@
-import { NavLink } from 'react-router-dom';
+import { NavLink } from "react-router-dom";
 
 function Nav() {
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-info">
       <div className="container-fluid">
-        <NavLink className="navbar-brand" to="/">Wardrobify</NavLink>
-        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <NavLink className="navbar-brand" to="/">
+          <img src={require("./images/hatshoe.png")} width="100" height="50" />{" "}
+        </NavLink>
+        <NavLink className="navbar-brand" to="/">
+          Fitted Sole
+        </NavLink>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
           <span className="navbar-toggler-icon"></span>
         </button>
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav me-auto mb-2 mb-lg-0">
             <li className="nav-item">
-              <NavLink className="nav-link active" aria-current="page" to="/">Home</NavLink>
+              <NavLink className="nav-link active" aria-current="page" to="/">
+                Home
+              </NavLink>
             </li>
             <li className="nav-item">
-              <NavLink className="nav-link" to="/bins">Create a New Bin</NavLink>
+              <NavLink className="nav-link" to="/bins">
+                Create a New Bin
+              </NavLink>
             </li>
             <li className="nav-item">
-              <NavLink className="nav-link" to="/shoes">Shoes</NavLink>
+              <NavLink className="nav-link" to="/shoes">
+                Shoes
+              </NavLink>
             </li>
             <li className="nav-item">
-              <NavLink className="nav-link" to="/shoes/new">Add New Shoes</NavLink>
+              <NavLink className="nav-link" to="/shoes/new">
+                Add New Shoes
+              </NavLink>
             </li>
             <li className="nav-item">
-              <NavLink className="nav-link" to="/locations">Create a New Location</NavLink>
+              <NavLink className="nav-link" to="/locations">
+                Create a New Location
+              </NavLink>
             </li>
             <li className="nav-item">
-              <NavLink className="nav-link" to="/hats">Hats</NavLink>
+              <NavLink className="nav-link" to="/hats">
+                Hats
+              </NavLink>
             </li>
             <li className="nav-item">
-              <NavLink className="nav-link" to="/hats/new">Add New Hats</NavLink>
+              <NavLink className="nav-link" to="/hats/new">
+                Add New Hats
+              </NavLink>
             </li>
           </ul>
         </div>
       </div>
     </nav>
-  )
+  );
 }
 
 export default Nav;
